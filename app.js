a25a2279d5817b0d3318e727ae9d50be7fed093b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-11 23:06:46 +0100

    Website rehaul

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const dirName = { root: __dirname };
+
+
+//might need to listen on IP instead of domain
+app.listen(80, 'megabytten.org');
+
+const sendHome = function (req, res) {
+  //res.send() is combination of both res.write() and res.end()
+  res.sendFile('./Main/Home/home.html', dirName);
+})
+
+//listens for any HTTP GET request on '/' || '/home' url
+app.get('/',  (req, res) => {
+  res.sendFile('./Main/Pages/Home/home.html', dirName);
+});
+app.get('/home', (req, res) => {
+  res.sendFile('./Main/Pages/Home/home.html', dirName);
+});
+
+app.get('/about', function (req, res) {
+  //res.send('about world!');
+  //res.sendFile() does not take a RELATIVE path, takes absolute
+  res.sendFile('./eutrcapp/verification.html', dirName);
+});
+
+//404 handler
+app.use( (req, res) => {
+  res.status(404).sendFile('./Main/Pages/404.html', dirName);
+});
---
+++ testapp.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const dirName = { root: __dirname };
+
+
+//might need to listen on IP instead of domain
+app.listen(3000);
+console.log('now Listening on port 3000');
+
+//listens for any HTTP GET request on '/' || '/home' url
+app.get('/',  (req, res) => {
+  res.sendFile('./Main/Pages/Home/home.html', dirName);
+});
+app.get('/home', (req, res) => {
+  res.sendFile('./Main/Pages/Home/home.html', dirName);
+});
+
+app.get('/about', function (req, res) {
+  //res.send('about world!');
+  //res.sendFile() does not take a RELATIVE path, takes absolute
+  res.sendFile('./eutrcapp/verification.html', dirName);
+});
+
+//404 handler
+app.use( (req, res) => {
+  res.status(404).sendFile('./Main/Pages/404.html', { root: __dirname});
+});
