5e14e1fabb3be4c7244ea8ea8d6fd1c728b89b96
BuildTools <unconfigured@null.spigotmc.org>
2022-07-11 23:45:02 +0100

    whoops forgot to change express .html response paths

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.listen(80, 'megabytten.org');
 
 //listens for any HTTP GET request on '/' || '/home' url
 app.get('/',  (req, res) => {
-  res.sendFile('./Main/Pages/Home/home.html', dirName);
+  res.sendFile('./home.html', dirName);
 });
 app.get('/home', (req, res) => {
-  res.sendFile('./Main/Pages/Home/home.html', dirName);
+  res.sendFile('./home.html', dirName);
 });
 
 app.get('/about', function (req, res) {
