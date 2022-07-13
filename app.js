cc22761dbefcc57e6a0c109838f23074b5e94c84
BuildTools <unconfigured@null.spigotmc.org>
2022-07-14 00:36:54 +0100

    +.py - Final Attempt!

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.post('/eutrcapp/verification', (req, res) => {
   const exec = require("child_process").exec;
   //const childPython = exec("type nul > filename.txt");
   console.log("Trace: " + "python verfbot " + emailBotSender + " " + emailBotPass + " " + userEmail);
-  const childPython = exec("python verfbot " + emailBotSender + " " + emailBotPass + " " + userEmail);
+  const childPython = exec("python verfbot.py " + emailBotSender + " " + emailBotPass + " " + userEmail);
   console.log("Trace childPython = " + childPython);
   childPython.stdout.on('data', (data) => {
   console.log('stdout: ${data}');
