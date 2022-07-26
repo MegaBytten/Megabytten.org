ed02012b7e59815827f952da33471d1fd0f0dd5e
BuildTools <unconfigured@null.spigotmc.org>
2022-07-26 13:45:08 +0400

    /?

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('/eutrcapp/verification', (req, res) => {
     console.log('async retrieve() called!');
     const verify = require('./EUTRCApp/verification.js');
     let userSQLPass = await verify.getUserPass(userEmail);
-    console.log('Trace stack: ' + userSQLPass + JSON.stringify(userSQLPass[0]));
+    console.log('Trace stack: ' + userSQLPass + JSON.stringify(userSQLPass.password));
 
     if (userSQLPass == null) {
       // User was not found in database, or incorrect email address provided.
