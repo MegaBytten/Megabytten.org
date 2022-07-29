235b220bba12e073f93cf6ee200a625095db51e9
MegaBytten <OmegaBytten@gmail.com>
2022-07-30 00:19:44 +0400

    Mysql.connection.end() too many connections whoops

--- EUTRCApp/verification.js
+++ EUTRCApp/verification.js
@@ -16,8 +16,9 @@ async function queryMySQL(query){
   // console.log(result);
   // console.table(result[0]);
   //result returns 2 arrays, first is the actual result and second is the schema info
-  console.log('Result[0] = ' + JSON.stringify(result[0])
-  );
+  console.log('Result[0] = ' + JSON.stringify(result[0]));
+  connection.end();
+
   return result[0];
 }
 
