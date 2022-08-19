4543ac1cf002fc63879dde84b795aab596efbc7d
MegaBytten <OmegaBytten@gmail.com>
2022-08-19 19:02:10 +0400

    MySQl Connction Storing  - fixed connection.end()

--- EUTRCApp/verification.js
+++ EUTRCApp/verification.js
@@ -40,7 +40,6 @@ async function queryMySQL(query){
   // console.table(result[0]);
   //result returns 2 arrays, first is the actual result and second is the schema info
   console.log('Result[0] = ' + JSON.stringify(result[0]));
-  connection.end();
 
   return result[0];
 }
