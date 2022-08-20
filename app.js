ea0e00e97fee4910d0628cf0b256bf3c6fa26fac
MegaBytten <OmegaBytten@gmail.com>
2022-08-20 16:39:52 +0400

    trainings/delete working update

--- app.js
+++ app.js
@@ -650,7 +650,49 @@ app.post('/eutrcapp/trainings/rsvp', async (req, res) => {
 //lined used to delete a training via trainingID from database
 app.post('/eutrcapp/trainings/delete', async (req, res) => {
   console.log(`\n\n/eutrcapp/trainings/delete reached! Attempting to delete training: ${req.body.trainingID}`);
-  
+  let userEmail = req.body.email;
+  let userPassword = req.body.password;
+  let trainingTable = 'training_' + req.body.trainingID;
+  let trainingID = req.body.trainingID;
+
+  let loginSuccess = await checkUserPassword(userEmail, userPassword);
+
+  //checks if user has correct login details
+  if (loginSuccess != 1){
+    console.log('login failed.');
+    res.status(998).send("failed login.");
+    return;
+  }
+
+  const verify = require('./EUTRCApp/verification.js');
+
+  //checks if user has COACH crediential (=1/true)
+  let query = `select coach from users where email = '${userEmail}';`
+  let userSQLResult = await verify.queryMySQL(query);
+  if (userSQLResult != null){
+    console.log("Coach status received! userSQLResult[0]['coach'] = " + userSQLResult[0]["coach"]);
+    let userSQL_coach = userSQLResult[0]["coach"];
+
+    if (userSQL_coach == 1){ //user confirmed coach
+      //user is coach! Delete training.
+      query = `delete from trainings where id = ${trainingID};`
+      verify.queryMySQL(query);
+
+      query = `drop table training_${trainingID};`
+      verify.queryMySQL(query);
+
+      console.log(`Successfully deleted training #${trainingID}`);
+      res.status(200).send('Successfully deleted training!')
+    } else {
+      console.log(`User ${userEmail} attempting to delete training #${trainingID}`);
+      res.status(998).send('Incorrect permissions.')
+    }
+  } else {
+    console.log("User's coach query was unsuccessfuly. Query return == null");
+    res.status(999).send('could not verify coach.')
+  }
+
+
 })
 
 
