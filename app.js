6e41f6056d35e5154d72ccfdcf469e1467bd5c49
MegaBytten <OmegaBytten@gmail.com>
2022-09-23 12:55:05 +0100

    changed ejs/status base to pure JS formatting

--- app.js
+++ app.js
@@ -204,13 +204,7 @@ app.post('/eutrcapp/verfbot', async (req, res) => {
       //login was a success
       console.log("Launching verfbot.py for user: " + userEmail);
       launchVerfBot(userEmail)
-
-      const verify = require('./EUTRCApp/verification.js');
-      const query = `SELECT first_name FROM users WHERE email = '${userEmail}';`
-      let sqlResult = await verify.queryMySQL(query);
-      let name = sqlResult[0]['first_name']
-      
-      res.status(200).render('./eutrc_app/verification.ejs', {name, email: userEmail, password:userPass, status:1})
+      res.status(200)
       break;
     case 2:
       //login was failure: Passwords did not match
